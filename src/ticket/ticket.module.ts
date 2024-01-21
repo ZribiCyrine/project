81752b3d1ab23d07ebb0c47559b4f7727f5e3852
Cyrine <cyrinezribi23"@gmail.com>
2024-01-21 17:04:34 +0100

    project

--- src/ticket/ticket.module.ts
+++ src/ticket/ticket.module.ts
@@ -5,9 +5,6 @@ import { Ticket } from '../entities/ticket.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Event } from '../entities/event.entity';
 import { EventService } from '../event/event.service';
-import { EventModule } from '../event/event.module';
-import { Participant } from '../entities/participant.entity';
-import { ParticipantService } from '../participant/participant.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Ticket, Event])],
